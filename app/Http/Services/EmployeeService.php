1ac10240e8fcff276eba9555fdbae62a3bf4fca7
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-02-14 16:42:23 +0530

    EmployeeService Created

---
+++ app/Http/Services/EmployeeService.php
@@ -0,0 +1,34 @@
+<?php
+
+class EmployeeService
+{
+    public function index()
+    {
+        return 'Employee Index';
+    }
+
+    public function create()
+    {
+        return 'Employee Create';
+    }
+
+    public function update()
+    {
+        return 'Employee Update';
+    }
+
+    public function delete()
+    {
+        return 'Employee Delete';
+    }
+
+    public function getById()
+    {
+        return 'Employee Get By Id';
+    }
+
+    public function getAll()
+    {
+        return 'Employee Get All';
+    }
+}
