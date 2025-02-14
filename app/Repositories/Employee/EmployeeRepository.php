b3bd54f00714ca27d733efd0348a8acf4a7e36c7
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-02-14 17:13:03 +0530

    repositories maked

---
+++ app/Repositories/Employee/EmployeeRepository.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace App\Repositories\Employee;
+class EmployeeRepository implements EmployeeRepositoryInterface
+{
+}
---
+++ app/Repositories/Employee/EmployeeRepositoryInterface.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace App\Repositories\Employee;
+interface EmployeeRepositoryInterface
+{
+}
