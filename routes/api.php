f6fc347eb8e47cc91cdd05c11753f84346bce376
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-02-21 12:34:41 +0530

    guard added

--- routes/api.php
+++ routes/api.php
@@ -8,20 +8,22 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-});
 
-Route::prefix('employee')->group(function () {
-    Route::post('', [EmployeeController::class, 'create']);
-    Route::patch('{id}', [EmployeeController::class, 'update']);
-    Route::delete('{id}', [EmployeeController::class, 'delete']);
-    Route::get('/getById/{id}', [EmployeeController::class, 'getById']);
-    Route::get('/getAll', [EmployeeController::class, 'getAll']);
-    Route::get('/getByAddress/{address}', [EmployeeController::class, 'getByAddress']);
-    Route::get('/getByEmail/{email}', [EmployeeController::class, 'getByEmail']);
-    Route::get('/test', [EmployeeController::class, 'test']);
-});
+    Route::group(['middleware' => ['auth:api']], function () {
+        Route::prefix('employee')->group(function () {
+            Route::post('', [EmployeeController::class, 'create']);
+            Route::patch('{id}', [EmployeeController::class, 'update']);
+            Route::delete('{id}', [EmployeeController::class, 'delete']);
+            Route::get('/getById/{id}', [EmployeeController::class, 'getById']);
+            Route::get('/getAll', [EmployeeController::class, 'getAll']);
+            Route::get('/getByAddress/{address}', [EmployeeController::class, 'getByAddress']);
+            Route::get('/getByEmail/{email}', [EmployeeController::class, 'getByEmail']);
+            Route::get('/test', [EmployeeController::class, 'test']);
+        });
 
-Route::prefix('staff')->group(function () {
-    Route::post('', [StaffController::class, 'create']);
-    Route::get('{id}', [StaffController::class, 'getById']);
+        Route::prefix('staff')->group(function () {
+            Route::post('', [StaffController::class, 'create']);
+            Route::get('{id}', [StaffController::class, 'getById']);
+        });
+    });
 });
