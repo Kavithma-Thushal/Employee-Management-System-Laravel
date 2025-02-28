52c3c6039e535d4e129d459b699bc1f7a1effd51
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-02-28 09:32:26 +0530

    Modified

--- config/common.php
+++ config/common.php
@@ -1,4 +1,9 @@
 <?php
 return [
     'generic_error' => 'error_message',
+    'imagesSizes' => [
+        ['name' => 'original', 'quality' => 100, 'convertable' => false],
+        ['name' => 'medium', 'quality' => 50, 'convertable' => true],
+        ['name' => 'thumbnail', 'quality' => 10, 'convertable' => true]
+    ]
 ];
